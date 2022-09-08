3e1b2d9fd76417139fb4ba198eacce8574086b8b
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-08 16:36:40 -0700

    putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
+#!/bin/bash
 #include <stdio.h>
-/**
- *
- *   main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
 
 int main(void)
 
