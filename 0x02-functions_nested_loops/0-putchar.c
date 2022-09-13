d7e54197e7a32fc4617ae2a56faba2e95e87dbbb
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-13 15:52:55 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,17 +9,15 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char word[] = "_putchar";
+	char *str = word;
 
-
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 	return (0);
 }
 
