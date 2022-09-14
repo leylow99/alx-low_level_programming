36a5cfea06d7af7e9bde5633a994f0380051d794
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-13 17:49:26 -0700

    at last

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints from a number to 98
+ * @n: the starting number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int dup = n;
+
+	do {
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+		else
+		{
+			printf("%d", n);
+			break;
+		}
+		n < 98 ? n++ : n--;
+	} while (n != 98);
+	if (dup != 98)
+		printf("98");
+	printf("\n");
+}
