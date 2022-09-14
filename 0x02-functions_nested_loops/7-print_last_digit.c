4c88ba96f9e5aa08689553a227c0d9152ba564d2
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-13 17:15:58 -0700

    digiit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to be tested
+ *
+ * Return: the last digit of the number
+ */
+
+int print_last_digit(int n)
+{
+	int last_num;
+
+	last_num = n % 10;
+	if (last_num < 0)
+		last_num *= -1;
+	_putchar('0' + last_num);
+	return (last_num);
+}
