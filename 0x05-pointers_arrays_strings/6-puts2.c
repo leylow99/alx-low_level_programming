21fa3e8127dda2ce870b40bc9e8d7dd276cce914
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-20 16:34:22 -0700

    puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: the string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	bool toggle = true;
+
+	while (*str)
+	{
+		if (toggle)
+			_putchar(*str);
+		str++;
+		toggle = !toggle;
+	}
+	_putchar('\n');
+}
