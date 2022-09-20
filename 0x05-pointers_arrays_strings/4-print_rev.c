e8da622a9a95986fe0b9558fc017f73cb10ac7ce
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-20 16:21:55 -0700

    preint-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
