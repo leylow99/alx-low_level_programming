28be9f6f682539cc4bcb54eb69e9faed3c1782bf
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-27 15:13:46 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - finds the first occurence of a char ina string
+ *  @s: the string
+ *  @c: the char
+ *  Return: the value
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++);
+	return (NULL);
+}
+         
