445b9a32a1cdc06f0278c3fd0ae53ff742d8c5de
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-27 15:02:12 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _memset - fills the first n bytes to a particular char
+ *   @s: the string
+ *   @b: the char
+ *   @n: the number of bytes
+ *   Return: char pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int num = 0;
+
+	for (; num < n; num++)
+		s[num] = b;
+	return (s);
+}
