890f54397ed58c322bd43b17a54a1656318d1c4b
Kalesanwo Olayinka <kalesanwoyinka99@gmail.com>
2022-09-30 21:34:44 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argurments
+ *  @argc: the lenght of the commandlinr
+ *  @argv: the command line arguements
+ *  Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
